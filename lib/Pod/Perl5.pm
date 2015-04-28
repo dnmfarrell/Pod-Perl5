93e05efa227662882f9e689b888f3e71dc2c20c5
David Farrell <davidnmfarrell@gmail.com>
2015-04-27 21:47:47 -0400

    version bump, interface tidy

--- lib/Pod/Perl5.pm
+++ lib/Pod/Perl5.pm
@@ -1,14 +1,19 @@
 use Pod::Perl5::Grammar;
 
-module Pod::Perl5:ver<0.03>
+module Pod::Perl5:ver<0.05>
 {
   our sub parse_file (Str:D $filepath)
   {
-    my $pod = $filepath.IO.slurp;
-    return parse_string($pod);
+    my $match = Pod::Perl5::Grammar.parsefile($filepath);
+
+    unless ($match)
+    {
+      die "Error parsing pod";
+    }
+    return $match;
   }
 
-  our sub parse_string (Str:D $pod, $action?)
+  our sub parse_string (Str:D $pod)
   {
     my $match = Pod::Perl5::Grammar.parse($pod);
 
